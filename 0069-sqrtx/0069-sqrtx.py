05673a66e8cdcc1226856c9c2ce2de07b4091c49
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2022-11-24 14:45:28 +0530

    Time: 42 ms (62.02%), Space: 13.4 MB (35.42%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,9 @@
+import math
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if(x>=0 and x<2,14,74,83,647):
+            return int(math.sqrt(x)) 
