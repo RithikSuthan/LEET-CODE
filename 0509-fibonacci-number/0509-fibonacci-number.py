43acfc615de5e70df585b1aacfbd25b8ddf2dd1d
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2022-11-24 16:38:02 +0530

    Time: 28 ms (69.35%), Space: 13.5 MB (35.56%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        ls=[0,1]
+        if(n==0):
+            return ls[0]
+        for i in range(n):
+            if(n==1):
+                return ls[1]
+            elif(n==2):
+                return ls[1]
+            else:
+                if(i==n-1):
+                    return ls[i+1]
+                ls.append(ls[i]+ls[i+1])
+                print(ls)
+
+        
